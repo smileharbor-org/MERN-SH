2a337715ea5f57a9b7cf4491363e0a5c7ee2e3b5
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-01-15 18:21:12 +0530

    outlet

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}>  {/* homepage route */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
