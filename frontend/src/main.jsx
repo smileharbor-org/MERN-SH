8cb7907055b29dfc36494ea8757e2f57576b25a3
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-01-17 18:30:36 +0530

    routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Contact from './components/contact/Contact.jsx'
+import Team from './components/team/Team.jsx'
+import Gallery from './components/gallery/Gallery.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App/>}>  {/* homepage route */}
+          <Route path='contact' element={<Contact/>}/> {/* contact route */}
+          <Route path='team' element={<Team/>}/> {/* team route */}
+          <Route path='gallery' element={<Gallery/>}/> {/* gallery route */}
         </Route>
       </Routes>
     </BrowserRouter>
