15025a37966dd19ea91bc9a9244777a8fdd5b639
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-01-19 16:27:03 +0530

    added bg image

--- frontend/src/components/contact/Contact.jsx
+++ frontend/src/components/contact/Contact.jsx
@@ -7,15 +7,31 @@ import {
   CardFooter,
 } from "@/components/ui/card"  // card
 import { Button } from '@/components/ui/button'  // button
+import bg from "./image/bg2.jpg"
 function Contact() {
   return (
     <>
-      <div className='w-full h-screen flex justify-center flex-col lg:flex-row lg:justify-around items-center'>
-        <div>
-          <h2 className=' text-[24px] lg:text-[32px] font-bold'>
+      <div className='w-full min-h-min lg:h-screen  flex justify-center flex-col lg:flex-row lg:justify-around items-center gap-3 lg:gap-0'
+        style={{
+          backgroundImage: `URL(${bg})`,
+          backgroundSize: 'cover', // Ensure the background covers the entire screen
+          backgroundPosition: 'center', // Center the background
+          backgroundRepeat: 'no-repeat', // Prevent repeating
+   
+        }}
+      >
+        <div className='flex flex-col items-center'>
+          <p className=' text-[20px] sm:text-[20px] md:text-[20px] lg:text-[32px] font-bold p-1'>
             Small acts of kindness lead to big changes.
-          </h2>
-          <h4 className='text-[18px] lg:text-[24px] font-semibold'>Let’s connect and create change together.</h4>
+          </p>
+          <h4 className='text-[20px] sm:text-[20px] md:text-[20px]  lg:text-[24px] font-semibold p-1'>Let’s connect and create change together.</h4>
+          {/* sample bg -> image */}
+          <img 
+            src="https://img.freepik.com/premium-vector/kindness-landing-page-flat-design_98292-25564.jpg"
+            width={400}
+            height={400}
+            className='rounded-lg p-1'
+          />
         </div>
         <Card className='w-[350px] lg:w-[500px] h-min py-6'>
           <form > {/*Form  */}
