918d777b15b0890957b6fc7d4f9a40a80e8db3a1
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-01-21 13:56:34 +0530

    initial setup

--- frontend/src/components/gallery/Gallery.jsx
+++ frontend/src/components/gallery/Gallery.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Gallery() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const fetchImages = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/cloudinary-images');
+      const imageUrls = response.data.map(image => image.secure_url);
+      setImages(imageUrls);
+    } catch (error) {
+      console.error("Error fetching images:", error);
+    }
+  };
+
   return (
-    <>
-        
-    </>
-  )
+    <div>
+      <h1>Cloudinary Image Gallery</h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {images.map((imageUrl, index) => (
+          <div key={index} style={{ margin: '10px' }}>
+            <img
+              src={imageUrl}
+              alt={`cloudinary-image-${index}`}
+              style={{ width: '200px', height: 'auto' }}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Gallery
+
+export default Gallery;
