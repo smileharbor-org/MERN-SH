8d87a44da67d7c920488b6cd606fc5ceb3d737fe
Drishyaat <drishyat2307@gmail.com>
2025-01-22 16:04:40 +0530

    added tailwind in navbar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,21 @@
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-    theme: {
-        extend: {
-            fontFamily: {
-                poppins: ['Poppins', 'sans-serif'], // Add Poppins font
-                roboto: ['Roboto', 'sans-serif'],// Add Roboto font
-            },
-            borderRadius: {
-                lg: 'var(--radius)',
-                md: 'calc(var(--radius) - 2px)',
-                sm: 'calc(var(--radius) - 4px)',
-            },
-            colors: {
-                // You can add custom colors here if needed
-            },
-        },
-    },
-    plugins: [require("tailwindcss-animate")],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+      extend: {
+          fontFamily: {
+              poppins: ['Poppins', 'sans-serif'], // Add Poppins font
+              roboto: ['Roboto', 'sans-serif'],// Add Roboto font
+          },
+          borderRadius: {
+              lg: 'var(--radius)',
+              md: 'calc(var(--radius) - 2px)',
+              sm: 'calc(var(--radius) - 4px)',
+          },
+          colors: {
+              // You can add custom colors here if needed
+          },
+      },
+  },
+  plugins: [require("tailwindcss-animate")],
 };
