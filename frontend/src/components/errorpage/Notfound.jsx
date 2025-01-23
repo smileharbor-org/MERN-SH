a582b204d075cb60b3fbc4c85140dea01d94a630
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-01-23 10:27:26 +0530

    notfound

--- frontend/src/components/errorpage/Notfound.jsx
+++ frontend/src/components/errorpage/Notfound.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import F1 from "./Images/F1.jpg"
+import error404 from "./Images/error404.jpg"
+
 
 function Notfound() {
   return (
     <>
-        
+      <div className=''>
+          <img src={F1} width={400} height={400} />
+      </div>
     </>
   )
 }
