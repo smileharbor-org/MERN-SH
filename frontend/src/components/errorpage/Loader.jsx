b11c57f5871f9b3647f3163711a8ba8c18629393
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-01-23 10:27:04 +0530

    loader

--- frontend/src/components/errorpage/Loader.jsx
+++ frontend/src/components/errorpage/Loader.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Loader1 from "./Images/Loader1.gif"
 
 function Loader() {
   return (
     <>
-        <h1>Loading...</h1>
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-lg font-bold mb-4">Loading...</h1>
+        <img src={Loader1} width={400} height={400} />
+      </div>
     </>
   )
 }
