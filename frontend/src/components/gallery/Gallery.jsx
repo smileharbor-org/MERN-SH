7cffd4ec82965972601d69dafda5ee61b4be0f41
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-01-24 20:35:51 +0530

    responsive

--- frontend/src/components/gallery/Gallery.jsx
+++ frontend/src/components/gallery/Gallery.jsx
@@ -19,22 +19,52 @@ function Gallery() {
   };
 
   return (
-    <div>
-      <h1>Cloudinary Image Gallery</h1>
-      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {images.map((imageUrl, index) => (
-          <div key={index} style={{ margin: '10px' }}>
-            <img
-              src={imageUrl}
-              alt={`cloudinary-image-${index}`}
-              style={{ width: '200px', height: 'auto' }}
-            />
-          </div>
-        ))}
+    <>
+      <div className='w-full h-min flex justify-center items-center'>
+        <h1 >Gallery</h1>
       </div>
-    </div>
+      <div>
+        <div
+          className="flex flex-wrap gap-4 p-4"
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "10px",
+            justifyContent: "center",
+          }}
+        >
+          {images.map((imageUrl, index) => (
+            <div
+              key={index}
+              style={{
+                border: "1px solid grey",
+                borderRadius: "10px",
+                overflow: "hidden", 
+                width: "230px", 
+                height: "230px",
+              }}
+            >
+              <picture>
+                <source srcSet={imageUrl} type='image/jpg' />
+                <img
+                  src={imageUrl}
+                  alt={` ${index}`}
+                  loading='lazy'
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                  }}
+                />
+              </picture>
+
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+
   );
 }
-
 
 export default Gallery;
