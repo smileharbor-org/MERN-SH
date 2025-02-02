8ac46dddea371e5fd3237fff6da5763490d902c8
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-02-02 16:54:22 +0530

    added notistack

--- frontend/src/components/contact/Contact.jsx
+++ frontend/src/components/contact/Contact.jsx
@@ -9,6 +9,7 @@ import {
 import { Button } from '@/components/ui/button'  // button
 import bg from "./image/bg.jpg"
 import { VITE_GOOGLE_SHEETS_API } from "../../config"
+import { SnackbarProvider, enqueueSnackbar } from 'notistack'
 function Contact() {
   const [formData, SetFormData] = useState({
     FullName: "",
@@ -25,6 +26,49 @@ function Contact() {
 
   const HandleForm = async (e) => {
     e.preventDefault()
+    if (!FullName) {
+      enqueueSnackbar("Full Name is required", { variant: "error",
+        anchorOrigin: {
+          vertical: 'top',
+          horizontal: 'right',
+        },
+        autoHideDuration:2000
+      });
+      return;
+    }
+    if (!email) {
+      enqueueSnackbar("Email is required", { variant: "error",
+        anchorOrigin: {
+          vertical: 'top',
+          horizontal: 'right',
+        },
+        autoHideDuration:2000
+
+       });
+      return;
+    }
+    if (!reasonForContact || reasonForContact === "Select a reason") {
+      enqueueSnackbar("Please select a reason for contact", { variant: "error", anchorOrigin: {
+        vertical: 'top',
+        horizontal: 'right',
+      } ,
+      autoHideDuration:2000
+
+    });
+      return;
+    }
+    if (!message) {
+      enqueueSnackbar("Message cannot be empty", {
+        variant: "error",
+        anchorOrigin: {
+          vertical: 'top',
+          horizontal: 'right',
+        },
+        autoHideDuration:2000
+
+      });
+      return;
+    }
     try {
       const result = await fetch(VITE_GOOGLE_SHEETS_API, {
         method: "POST",
@@ -63,64 +107,67 @@ function Contact() {
           <h4 className='text-[20px] sm:text-[20px] md:text-[20px]  lg:text-[24px] font-semibold p-1'>Let’s connect and create change together.</h4>
 
         </div>
-        <Card className='w-[350px] lg:w-[500px] h-min py-6'>
-          <form onSubmit={HandleForm}> {/*Form  */}
-            <CardContent>
-              <div className="grid w-full  items-center gap-2 my-5">
-                <Label htmlFor="email" className="text-[16px] lg:text-[24px]">FullName</Label>
-                <Input
-                  type="text"
-                  id="FullName"
-                  name="FullName"
-                  value={FullName}
-                  onChange={HandleChange}
-                  placeholder="FullName" />
-              </div>
-              <div className="grid w-full  items-center gap-2 my-5">
-                <Label htmlFor="email" className="text-[16px] lg:text-[24px]">Email</Label>
-                <Input
-                  type="email"
-                  name="email"
-                  value={email}
-                  onChange={HandleChange}
+        <SnackbarProvider maxSnack={30}>  {/**Notifications */}
+          <Card className='w-[350px] lg:w-[500px] h-min py-6'>
+            <form onSubmit={HandleForm}> {/*Form  */}
+              <CardContent>
+                <div className="grid w-full  items-center gap-2 my-5">
+                  <Label htmlFor="email" className="text-[16px] lg:text-[24px]">FullName</Label>
+                  <Input
+                    type="text"
+                    id="FullName"
+                    name="FullName"
+                    value={FullName}
+                    onChange={HandleChange}
+                    placeholder="FullName" />
+                </div>
+                <div className="grid w-full  items-center gap-2 my-5">
+                  <Label htmlFor="email" className="text-[16px] lg:text-[24px]">Email</Label>
+                  <Input
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={HandleChange}
 
-                  placeholder="Email" />
-              </div>
-              <div>
-                <Label htmlFor="reasonForContact" className="text-[16px] lg:text-[24px]" >Reason for Contact</Label>
-                <select
-                  name="reasonForContact"
-                  value={reasonForContact}
-                  onChange={HandleChange}
+                    placeholder="Email" />
+                </div>
+                <div>
+                  <Label htmlFor="reasonForContact" className="text-[16px] lg:text-[24px]" >Reason for Contact</Label>
+                  <select
+                    name="reasonForContact"
+                    value={reasonForContact}
+                    onChange={HandleChange}
 
-                  className="border rounded-sm w-full p-2 bg-white"
-                  required
-                >
-                  <option>Select a reason</option>
-                  <option value="donate">Donate</option>
-                  <option value="volunteer">Volunteer</option>
-                  <option value="other">Other</option>
-                </select>
-              </div>
-              <div className="grid w-full  items-center gap-2 my-5">
-                <Label htmlFor="email" className="text-[16px] lg:text-[24px]">Message</Label>
-                <textarea
-                  rows={5}
-                  type="text"
+                    className="border rounded-sm w-full p-2 bg-white"
+                    required
+                  >
+                    <option>Select a reason</option>
+                    <option value="donate">Donate</option>
+                    <option value="volunteer">Volunteer</option>
+                    <option value="other">Other</option>
+                  </select>
+                </div>
+                <div className="grid w-full  items-center gap-2 my-5">
+                  <Label htmlFor="email" className="text-[16px] lg:text-[24px]">Message</Label>
+                  <textarea
+                    rows={5}
+                    type="text"
 
-                  name='message'
-                  value={message}
-                  onChange={HandleChange}
+                    name='message'
+                    value={message}
+                    onChange={HandleChange}
 
-                  placeholder="Message"
-                  className='border rounded-sm' />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button type="submit">Send</Button>
-            </CardFooter>
-          </form>
-        </Card>
+                    placeholder="Message"
+                    className='border rounded-sm' />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button type="submit" className="w-full">Send</Button>
+              </CardFooter>
+            </form>
+          </Card>
+        </SnackbarProvider>
+
       </div>
 
     </>
