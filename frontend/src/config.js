a5a4d10dee039efac8ca2007447677d4ffd97441
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-02-02 16:39:35 +0530

    razorpay

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -5,3 +5,6 @@ export const VITE_TWITTER = import.meta.env.VITE_TWITTER
 export const VITE_YOUTUBE = import.meta.env.VITE_YOUTUBE
 export const VITE_LINKEDIN = import.meta.env.VITE_LINKEDIN
 export const VITE_GOOGLE_SHEETS_API  = import.meta.env.VITE_GOOGLE_SHEETS_API 
+export const VITE_RAZORPAY_KEY  = import.meta.env.VITE_RAZORPAY_KEY 
+export const VITE_RAZORPAT_SECRET  = import.meta.env.VITE_RAZORPAT_SECRET 
+
