e0f70542d296a3be0bc9f500ba92e2a074bf2783
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-02-03 12:24:32 +0530

    donate api

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -7,4 +7,5 @@ export const VITE_LINKEDIN = import.meta.env.VITE_LINKEDIN
 export const VITE_GOOGLE_SHEETS_API  = import.meta.env.VITE_GOOGLE_SHEETS_API 
 export const VITE_RAZORPAY_KEY  = import.meta.env.VITE_RAZORPAY_KEY 
 export const VITE_RAZORPAT_SECRET  = import.meta.env.VITE_RAZORPAT_SECRET 
+export const VITE_GOOGLE_DONATION  = import.meta.env.VITE_GOOGLE_DONATION
 
