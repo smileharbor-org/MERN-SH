417a3d2d9a88ad971f8599d900dd93d6bdc3d0ee
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-02-03 12:19:13 +0530

    Sheet DB Setup with notification

--- frontend/src/components/donate/Donate.jsx
+++ frontend/src/components/donate/Donate.jsx
@@ -9,26 +9,28 @@ import {
 } from "@/components/ui/card"  // card
 import { Button } from '@/components/ui/button'  // button
 import { IndianRupee } from 'lucide-react';
-import video from "./contents/donate.mp4"
+// import video from "./contents/donate.mp4"
 import { SnackbarProvider, enqueueSnackbar } from 'notistack'
-import {VITE_RAZORPAT_SECRET,VITE_RAZORPAY_KEY} from "../../config"
+// import {VITE_RAZORPAT_SECRET,VITE_RAZORPAY_KEY} from "../../config"
+import {VITE_GOOGLE_DONATION} from "../../config"
 
 function Donate() {
   const [DataValues, SetDataValues] = useState({
     fullname: "",
     email: "",
     number: "",
-    amount: ""
+    message: ""
   })
-  const { fullname, email, amount, number } = DataValues
+  const { fullname, email, message, number } = DataValues
   const HandleChange = (e) => {
     SetDataValues({ ...DataValues, [e.target.name]: e.target.value })
   }
   //Function for Submit Pay
-  const HandleSubmit = (e) => {
+  const HandleSubmit = async(e) => {
     e.preventDefault()
-    if (!fullname || !email || !amount) {
-      enqueueSnackbar("Check All Credentials", {
+    const emailVerification = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!fullname) {
+      enqueueSnackbar("Enter Your Name", {
         variant: "warning",
         autoHideDuration: 2000,
         anchorOrigin: {
@@ -36,55 +38,157 @@ function Donate() {
           horizontal: 'right',
         }
       })
-    } else {
-
-      const options = {
-        key: VITE_RAZORPAY_KEY,
-        key_secret: VITE_RAZORPAT_SECRET,
-        amount: amount * 100,
-        currency: "INR",
-        name: "SmileHarbor",
-        description: "Donation",
-        handler: function (res) {
-          if (res) {
-            enqueueSnackbar("Thank you!! Payment Successfull", {
-              variant: "success",
-              autoHideDuration: 2000,
-              anchorOrigin: {
-                vertical: 'top',
-                horizontal: 'right',
-              }
-            })
-            SetDataValues({
-              fullname: "",
-              email: "",
-              number: "",
-              amount: ""
-            })
-          }
-        },
-        prefill: {
-          name: fullname,
-          email: email,
-          contact: number
-        },
-        notes: {
-          address: "simleharbor"
-        },
-        theme: {
-          color: "green"
-        }
-      }
-      const pay = new window.Razorpay(options)
-      pay.open()
-
-
+      return
     }
+    if (!email) {
+      enqueueSnackbar("Enter Your Email", {
+        variant: "warning",
+        autoHideDuration: 2000,
+        anchorOrigin: {
+          vertical: 'top',
+          horizontal: 'right',
+        }
+      })
+
+      return
+    }
+    if (!emailVerification.test(email)) {
+      enqueueSnackbar("Enter Your Correct Email", {
+        variant: "warning",
+        autoHideDuration: 2000,
+        anchorOrigin: {
+          vertical: 'top',
+          horizontal: 'right',
+        }
+      })
+      return
+    }
+
+
+
+    if (number.length > 11 || number.length < 10) {
+      enqueueSnackbar("Enter Correct Mobile Number", {
+        variant: "warning",
+        autoHideDuration: 2000,
+        anchorOrigin: {
+          vertical: 'top',
+          horizontal: 'right',
+        }
+      })
+      return
+    }
+    if (!message) {
+      enqueueSnackbar("Enter Your Message", {
+        variant: "warning",
+        autoHideDuration: 2000,
+        anchorOrigin: {
+          vertical: 'top',
+          horizontal: 'right',
+        }
+      })
+      return
+    }
+    //Google sheet api by DB Sheet
+    try {
+      const DateTime = new Date().toLocaleString()
+      const FormData = {
+        TimeStamp:DateTime,...DataValues
+      }
+      const res = await fetch(VITE_GOOGLE_DONATION, {
+        method: "POST",
+        headers: { "Content-Type": "application/json",
+          Accept:"application/json"
+         },
+        body: JSON.stringify(FormData),
+        
+      });
+  
+      const data = await res.json();
+      console.log(data)
+      if (data){
+        enqueueSnackbar("Thank you for donation request. We get back to you", {
+          variant: "success",
+          autoHideDuration: 2000,
+          anchorOrigin: {
+            vertical: 'top',
+            horizontal: 'right',
+          }
+        })
+      }else {
+        enqueueSnackbar("Submission Failed.Try Again", {
+          variant: "warning",
+          autoHideDuration: 2000,
+          anchorOrigin: {
+            vertical: 'top',
+            horizontal: 'right',
+          }
+        })
+      }
+
+    } catch (error) {
+      console.log(error)
+      if (error) {
+        enqueueSnackbar("Try Again Later", {
+          variant: "warning",
+          autoHideDuration: 2000,
+          anchorOrigin: {
+            vertical: 'top',
+            horizontal: 'right',
+          }
+        })
+      }
+    }
+
+
+    // else {
+    //   //PAYMENT GATEWAY SETUP
+    //   const options = {
+    //     key: VITE_RAZORPAY_KEY,
+    //     key_secret: VITE_RAZORPAT_SECRET,
+    //     amount: amount * 100,
+    //     currency: "INR",
+    //     name: "SmileHarbor",
+    //     description: "Donation",
+    //     handler: function (res) {
+    //       if (res) {
+    //         enqueueSnackbar("Thank you!! Payment Successfull", {
+    //           variant: "success",
+    //           autoHideDuration: 2000,
+    //           anchorOrigin: {
+    //             vertical: 'top',
+    //             horizontal: 'right',
+    //           }
+    //         })
+    //         SetDataValues({
+    //           fullname: "",
+    //           email: "",
+    //           number: "",
+    //           amount: ""
+    //         })
+    //       }
+    //     },
+    //     prefill: {
+    //       name: fullname,
+    //       email: email,
+    //       contact: number
+    //     },
+    //     notes: {
+    //       address: "simleharbor"
+    //     },
+    //     theme: {
+    //       color: "green"
+    //     }
+    //   }
+    //   const pay = new window.Razorpay(options)
+    //   pay.open()
+
+
+    // }
   }
   return (
     <>
 
-      <div className="absolute top-0 left-0 w-full h-full z-0" >
+      {/* <div className="absolute top-0 left-0 w-full h-full z-0" >
         <video
           autoPlay
           loop
@@ -93,7 +197,7 @@ function Donate() {
         >
           <source src={video} type="video/mp4" />
         </video>
-      </div>
+      </div> */}
       <div className='flex justify-around items-center flex-col lg:flex-row  h-screen relative z-20 '
       >
         <h2 className='text-[24px] sm:text-[21px] md:text-[22px] lg:text-[32px] max-w-[350px] font-bold'>
@@ -104,10 +208,10 @@ function Donate() {
             <form onSubmit={HandleSubmit}>
               <CardContent>
                 <CardTitle>
-                  Fill Form To Donate
+                  PLEDGE TO DONATE
                 </CardTitle>
                 <div className="grid w-full  items-center gap-2 my-5">
-                  <Label htmlFor="email" className="text-[16px] lg:text-[24px]">Full Name</Label>
+                  <Label htmlFor="email" className="text-[16px] lg:text-[24px]">Full Name </Label>
                   <Input
                     id="fullname"
                     value={fullname}
@@ -137,24 +241,21 @@ function Donate() {
                   />
                 </div>
                 <div className="grid w-full  items-center gap-2 my-5">
-                  <Label htmlFor="email" className="text-[16px] lg:text-[24px]">Amount</Label>
-                  <div className='flex items-center gap-1'>
-                    <IndianRupee />
-                    <Input
-                      type="text"
-                      id="amount"
-                      name="amount"
-                      value={amount}
-                      onChange={HandleChange}
-                    />
-                  </div>
-
+                  <Label htmlFor="message" className="text-[16px] lg:text-[24px]">Message</Label>
+                  <textarea
+                    rows={5}
+                    type="text"
+                    className=' border rounded-sm'
+                    id="message"
+                    name="message"
+                    value={message}
+                    onChange={HandleChange}
+                  />
                 </div>
-
+                <h4 className='text-[12px] font-bold'>* Currently we not accepting donation.Shortly we get back to you.</h4>
               </CardContent>
               <CardFooter >
-                <Button type="submit" className="w-full">Pay Now</Button>
-
+                <Button type="submit" className="w-full">Send</Button>
               </CardFooter>
             </form>
 
