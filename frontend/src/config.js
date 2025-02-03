ccb92e962bbdaed1a593f2717bb68a11eca81f12
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-02-03 14:55:27 +0530

    email env

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -8,4 +8,7 @@ export const VITE_GOOGLE_SHEETS_API  = import.meta.env.VITE_GOOGLE_SHEETS_API
 export const VITE_RAZORPAY_KEY  = import.meta.env.VITE_RAZORPAY_KEY 
 export const VITE_RAZORPAT_SECRET  = import.meta.env.VITE_RAZORPAT_SECRET 
 export const VITE_GOOGLE_DONATION  = import.meta.env.VITE_GOOGLE_DONATION
+export const VITE_EMAILJS_TEMPLATE  = import.meta.env.VITE_EMAILJS_TEMPLATE
+export const VITE_SERVICE_ID  = import.meta.env.VITE_SERVICE_ID
+export const VITE_PUBLIC_KEY  = import.meta.env.VITE_PUBLIC_KEY
 
