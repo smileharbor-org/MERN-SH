91e8e7b6769929ba4a97fe41c0a3a84afe4ceaaf
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-02-22 15:43:34 +0530

    helmet provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,24 +7,27 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Loader from './components/errorpage/Loader.jsx'
 const Donate = lazy(() => import("./components/donate/Donate.jsx"))
 const Notfound = lazy(() => import("./components/errorpage/Notfound.jsx"))
-const Gallery = lazy(()=>import('./components/gallery/Gallery.jsx'))
-const Contact = lazy(() =>import('./components/contact/Contact.jsx'))
-const Team = lazy(()=>import('./components/team/Team.jsx'))
-
+const Gallery = lazy(() => import('./components/gallery/Gallery.jsx'))
+const Contact = lazy(() => import('./components/contact/Contact.jsx'))
+const Team = lazy(() => import('./components/team/Team.jsx'))
+import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Suspense fallback={<Loader/>}>
-        <Routes>
-          <Route path='/' element={<App />}>  {/* homepage route */}
-            <Route path='contact' element={<Contact />} /> {/* contact route */}
-            <Route path='team' element={<Team />} /> {/* team route */}
-            <Route path='gallery' element={<Gallery />} /> {/* gallery route */}
-            <Route path='donate' element={<Donate />} /> {/*  donate */}
-            <Route path='*' element={<Notfound />} />{/* 404 */}
-          </Route>
-        </Routes>
+      <Suspense fallback={<Loader />}>
+        <HelmetProvider>
+          <Routes>
+            <Route path='/' element={<App />}>  {/* homepage route */}
+              <Route path='contact' element={<Contact />} /> {/* contact route */}
+              <Route path='team' element={<Team />} /> {/* team route */}
+              <Route path='gallery' element={<Gallery />} /> {/* gallery route */}
+              <Route path='donate' element={<Donate />} /> {/*  donate */}
+              <Route path='*' element={<Notfound />} />{/* 404 */}
+            </Route>
+          </Routes>
+        </HelmetProvider>
+
       </Suspense>
 
     </BrowserRouter>
