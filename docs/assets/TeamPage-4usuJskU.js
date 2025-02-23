e59f17f2fc4889a8387c5e667c3c610030937560
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-02-23 15:38:30 +0530

    update5

---
+++ docs/assets/TeamPage-4usuJskU.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Team-Cyp7wGuO.js","assets/index-Aba-m4Or.js","assets/index-quLRHvb7.css"])))=>i.map(i=>d[i]);
+import{r as a,_ as r,j as e,L as s}from"./index-Aba-m4Or.js";const t=a.lazy(()=>r(()=>import("./Team-Cyp7wGuO.js"),__vite__mapDeps([0,1,2])));function o(){return e.jsx(e.Fragment,{children:e.jsx(a.Suspense,{fallback:e.jsx(s,{}),children:e.jsx(t,{})})})}export{o as default};
