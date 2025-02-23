648b48a1fbf6295b788baca205b4edffa10a2c61
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-02-23 14:37:33 +0530

    loader

--- frontend/src/components/team/TeamPage.jsx
+++ frontend/src/components/team/TeamPage.jsx
@@ -1,11 +1,12 @@
 import React, { lazy, Suspense } from 'react'
 import Team from './Team'
+import Loader from '../errorpage/Loader'
 const TeamPage = lazy(()=>import("./Team"))
 
 function TeamPage() {
   return (
     <>
-        <Suspense fallback={<div>...</div>}>
+        <Suspense fallback={<Loader/>}>
             <Team/>
         </Suspense>
     </>
