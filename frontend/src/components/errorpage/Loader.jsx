90725c34f5fb3325c34d93644c73d381b289ce47
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-02-23 14:29:26 +0530

    fixed

--- frontend/src/components/errorpage/Loader.jsx
+++ frontend/src/components/errorpage/Loader.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Loader() {
   return (
     <>
-      <div className="flex justify-center items-center h-60">
+      <div className="flex justify-center items-center h-screen">
         <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
       </div>
     </>
